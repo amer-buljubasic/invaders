72cc62526d0a2b1b3bd020a9b8d8dc72665f4256
amer-buljubasic <buljubasic.amer@gmail.com>
2025-11-20 21:41:06 +0100

    Initial dev commit

---
+++ lib/code_invaders.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "code_invaders/version"
+
+module CodeInvaders
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/code_invaders/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CodeInvaders
+  VERSION = "0.1.0"
+end
