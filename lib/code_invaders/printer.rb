e4a5514692a88689c76683c4a5b4bce0088d7cd9
amer-buljubasic <buljubasic.amer@gmail.com>
2025-11-23 16:21:25 +0100

    Show message when no invaders found

--- lib/code_invaders/printer.rb
+++ lib/code_invaders/printer.rb
@@ -1,6 +1,8 @@
 module CodeInvaders
   class Printer
     def self.print_results(results:)
+      p "No ivaders found. We are safe!" if results == {}
+
       results.each do |k, v|
         p "Invader #{k} found at locations: #{v}"
       end
