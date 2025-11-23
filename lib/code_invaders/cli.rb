15f79b362bb6bfbe33edc03fb9f2a090296afa2f
amer-buljubasic <buljubasic.amer@gmail.com>
2025-11-23 16:32:16 +0100

    Exclude cops

--- lib/code_invaders/cli.rb
+++ lib/code_invaders/cli.rb
@@ -11,22 +11,27 @@ module CodeInvaders
     end
 
     def call
-      if @argv.empty?
-        puts "Error: No input file provided."
-        puts "Usage: code_invaders path/to/file.txt"
-        exit 1
-      end
+      return file_missing if @argv.empty?
 
       file_path = @argv.first
-
-      unless File.exist?(file_path)
-        puts "File not found: #{file_path}"
-        exit 1
-      end
+      return file_not_exist(file_path) unless File.exist?(file_path)
 
       radar_sample = File.read(file_path)
 
       CodeInvaders.track(radar_sample:)
+    end
+
+    private
+
+    def file_missing
+      puts "Error: No input file provided."
+      puts "Usage: code_invaders path/to/file.txt"
+      exit 1
+    end
+
+    def file_not_exist(file_path)
+      puts "File not found: #{file_path}"
+      exit 1
     end
   end
 end
